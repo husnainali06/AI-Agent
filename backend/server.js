76ef6f5780e5061189dc60fd44f1dd6f84eedfdf
bextoratechsolutions-code <bextoratechsolutions@gmail.com>
2026-07-31 19:18:20 +0500

    setup backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,38 @@ import connectDB from "./config/db.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
+
 const app = express();
+
 
 // Connect MongoDB
 connectDB();
 
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://ai-agent-psi-one.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
+
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 
+
 // Test Route
-app.get("/", (req, res) => {
+app.get("/", (req,res)=>{
   res.json({
-    success: true,
-    message: "AI Agent Backend Running 🚀",
+    success:true,
+    message:"AI Agent Backend Running 🚀"
   });
 });
 
-// Export for Vercel
+
 export default app;
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,9 +3,39 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
 
 });
+
+
+// Token automatically send karne ke liye
+api.interceptors.request.use(
+
+  (config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+
+      config.headers.Authorization = `Bearer ${token}`;
+
+    }
+
+    return config;
+
+  },
+
+  (error) => {
+
+    return Promise.reject(error);
+
+  }
+
+);
 
 
 export default api;
